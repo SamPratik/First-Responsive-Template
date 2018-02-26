0c7be050eeb496a954414579b43a8978e3d5e4dc
SamPratik <pratik.anwar@gmail.com>
2018-02-26 15:30:24 +0600

    template completed

---
+++ index.php
@@ -0,0 +1,481 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+<title>Entrepreneur Show begins ;-)</title>
+
+<link type="text/css" rel="stylesheet" href="CSS/header.css">
+<link type="text/css" rel="stylesheet" href="CSS/aboutUs.css">
+<link type="text/css" rel="stylesheet" href="CSS/learnTheme.css">
+<link type="text/css" rel="stylesheet" href="CSS/gallery.css">
+<link type="text/css" rel="stylesheet" href="CSS/contact.css">
+</head>
+
+<body>
+
+	<div class="container">
+    	<a name="home"></a>
+    	<div class="menu-icon-items">
+        	<a href="#home" class="menuText smoothScroll">Home</a>
+            <a href="#about" class="menuText smoothScroll">About</a>
+            <a href="#gallery" class="menuText smoothScroll">Gallery</a>
+            <a href="#contact" class="last-child menuText smoothScroll">Contact</a>
+        </div>
+    
+    	<!--------div.header-------->
+    	<div class="header">
+        
+        	<h2 class="brand-name">Entrepreneur</h2>
+            
+            <span class="menu-icon" id="menuIconId"></span>
+        	
+        	<div id="menuBarId" class="menu-bar">
+            	<a class="active smoothScroll" href="#home">Home</a>
+                <a class="smoothScroll" href="#about">About</a>
+                <a class="smoothScroll" href="#gallery">Gallery</a>
+                <a class="smoothScroll" href="#contact">Contact</a>
+            </div>
+            
+            <div class="header-cover"></div>
+            
+        </div><!--------div.header-------->
+          
+        <!--------div.header-img-------->  
+        <div class="header-img" id="headerImageId">
+            <div class="header-img-inner"></div>
+            
+            <div class="header-img-content">
+                <h1>CREATE YOUR OWN COMPANY</h1>
+                <h3>Have Your Own Product & Be An Entrepreneur</h3>
+            </div>
+            
+            <a href="#" id="headerButtonId" class="header-button">Demo Button</a>
+            
+        </div><!--------div.header-img-------->
+        
+        <!-------div.about-us-container------->
+        <div class="about-us-container">
+        	<a name="about"></a>
+        	<div class="about-us-content">
+            	<h1>ABOUT US</h1>
+                
+                <div class="about-us-thumbnail">
+                	<img class="about-us-img" src="images/converted idols pic/250X250.jpg">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque rutrum mi iaculis, eleifend nisl ut, fringilla turpis. Nunc efficitur, ante ut bibendum ullamcorper, dui sem finibus nisi, eget ultrices tellus eros et est. Ut posuere tellus lectus, sed vulputate diam maximus elementum.</p>
+                </div>
+                <div class="about-us-thumbnail">
+                	<img class="about-us-img" src="images/converted idols pic/250X250Steve.jpg">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque rutrum mi iaculis, eleifend nisl ut, fringilla turpis. Nunc efficitur, ante ut bibendum ullamcorper, dui sem finibus nisi, eget ultrices tellus eros et est. Ut posuere tellus lectus, sed vulputate diam maximus elementum.</p>
+                </div>
+                <div class="about-us-thumbnail">
+                	<img class="about-us-img" src="images/converted idols pic/250X250Allen.jpg">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque rutrum mi iaculis, eleifend nisl ut, fringilla turpis. Nunc efficitur, ante ut bibendum ullamcorper, dui sem finibus nisi, eget ultrices tellus eros et est. Ut posuere tellus lectus, sed vulputate diam maximus elementum.</p>                   
+                </div>
+                
+                <div style="clear:both"></div>
+                
+            </div><!--------div.about-us-content----->
+            
+            <div style="clear:both"></div>
+            
+        </div><!-------div.about-us-container------->
+        
+        <hr>
+        
+        <!---------div.learn-about-theme--------->
+        <div class="learn-about-theme">
+        
+        	<div class="points-container"><!--------div.points-container---------->
+            	<p id="demo"></p>
+                <div class="points">
+                    <h2><span class="point-number">&#10122;</span>Learn About First Point</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vel rutrum erat. Phasellus a elit eget tortor porta aliquam. Pellentesque vel enim rutrum, sagittis ante sed, tempus lacus.</p>
+                </div>
+                
+                <div class="points">
+                    <h2><span class="point-number">&#10103;</span>Learn About Second Point</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vel rutrum erat. Phasellus a elit eget tortor porta aliquam. Pellentesque vel enim rutrum, sagittis ante sed, tempus lacus.</p>
+                </div>
+                                
+                <div class="points">
+                    <h2><span class="point-number">&#10104;</span>Learn About Third Point</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vel rutrum erat. Phasellus a elit eget tortor porta aliquam. Pellentesque vel enim rutrum, sagittis ante sed, tempus lacus.</p>
+                </div>
+                
+            </div><!--------div.points-container---------->
+            
+            <!----------img.learn-theme-image------------>
+            <div>
+            	<img class="learn-theme-image" src="images/convertedLearnThemeImage.jpg">
+            </div>
+            
+        </div><!---------div.learn-about-theme---------->
+        
+        <!---------learn-about-theme-behind-bcg--------->
+        <div id="learnAboutThemeBehindBcgId" class="learn-about-theme-behind-bcg">
+        	<div class="learn-about-theme-behind-bcg-cover"></div>
+        </div>
+        
+        <!----------div.header-over-bcg---------->
+        <!--<div class="header-over-bcg">-->
+        <h1 class="header-over-bcg">Entrepreneurship is a <span>lifestyle</span></h1>
+        <!--</div>--><!----------div.header-over-bcg---------->
+        
+        
+        <!--------div.gallery--------->
+        <div class="gallery">
+        
+        	<a name="gallery"></a>
+        	
+            <!---------div.gallery-heading-------->
+            <h1>Image Gallery</h1>
+            
+            <div style="clear:both;"></div>
+            
+        	<!--------div.row1-------->
+        	<div class="row">
+                <div class="img-container">
+                    <img src="images/converted gallery/img1.jpg">
+                    <p>Lorem ipsum dolor, amet</p>
+                </div>
+                
+                <div class="clear1"></div>
+                
+                <div class="img-container">
+                    <img src="images/converted gallery/imgjpg10.jpg">
+                    <p>Lorem ipsum dolor, amet</p>
+                </div>
+                
+                <div class="clear1"></div>
+              	<div class="clear2"></div>
+                
+                <div class="img-container">
+                    <img src="images/converted gallery/imgjpg12.jpg">
+                    <p>Lorem ipsum dolor, amet</p>
+                </div>
+                
+                <div class="clear1"></div>
+                
+                <div class="img-container">
+                    <img src="images/converted gallery/imgjpg5.jpg">
+                    <p>Lorem ipsum dolor, amet</p>
+                </div>
+                
+                <div class="clear1"></div>
+                <div class="clear2"></div>
+                <div class="clear4"></div>
+
+                <div class="img-container">
+                    <img src="images/converted gallery/imgjpg3.jpg">
+                    <p>Lorem ipsum dolor, amet</p>
+                </div>
+                
+                <div class="clear1"></div>
+                
+                <div class="img-container">
+                    <img src="images/converted gallery/imgjpg9.jpg">
+                    <p>Lorem ipsum dolor, amet</p>
+                </div>
+                
+                <div class="clear1"></div>
+                <div class="clear2"></div>
+                <div class="clear3"></div>
+                
+                <div class="img-container">
+                    <img src="images/converted gallery/imgjpg11.jpg">
+                    <p>Lorem ipsum dolor, amet</p>
+                </div>
+                
+                <div class="clear1"></div>
+                
+                <div class="img-container">
+                    <img src="images/converted gallery/imgjpg8.jpg">
+                    <p>Lorem ipsum dolor, amet</p>
+                </div>  
+                
+                <div class="clear1"></div>
+                <div class="clear2"></div>
+            	<div class="clear4"></div>   
+                
+                <div class="img-container">
+                    <img src="images/converted gallery/newImg1.jpg">
+                    <p>Lorem ipsum dolor, amet</p>
+                </div>
+                
+                <div class="clear1"></div>
+                <div class="clear3"></div>
+                
+                <div class="img-container">
+                    <img src="images/converted gallery/newImg2.jpg">
+                    <p>Lorem ipsum dolor, amet</p>
+                </div>
+                
+                <div class="clear1"></div>
+                <div class="clear2"></div>
+                
+                <div class="img-container">
+                    <img src="images/converted gallery/newImg3.jpg">
+                    <p>Lorem ipsum dolor, amet</p>
+                </div>
+                
+                <div class="clear1"></div>
+                
+                <div class="img-container">
+                    <img src="images/converted gallery/imgjpg8.jpg" onClick="showModal()">
+                    <p>Lorem ipsum dolor, amet</p>
+                </div>
+                
+                <div class="clear1"></div>
+                <div class="clear2"></div>
+                <div class="clear3"></div>
+                <div class="clear4"></div>   
+                
+             </div><!--------div.row--------->                                    
+            
+        </div><!--------div.gallery--------->       
+        
+        <div class="contact">
+        	<a name="contact"></a>
+        	<div class="logo">
+				<h2>Entrepreneur</h2>
+            </div>
+             
+            <div class="some-page">
+             	
+                <div class="anchor-container">
+                	<h2>Some Pages</h2>
+                	<a href="">Home</a>
+                    <a href="">View some works here</a>
+                    <a href="">Follow our blog</a>
+                    <a href="">Contact us</a>
+                    <a href="">Our service</a>
+                </div>           
+            </div>       
+            
+            <div class="recent-post">
+            	<div class="anchor-container">
+                    <h2>Recent posts</h2>
+    
+                    <a href="">Hello World!</a>
+                    <a href="">This is the post title here</a>
+                    <a href="">Our happy day</a>
+                    <a href="">The first works done</a>
+                    <a href="">The cats and dogs</a>
+                </div>
+            </div>
+            
+            <div class="email">
+                <div class="email-container">
+                    <h2>Email updates</h2>
+                    <p>We want to share our latest trends, news and insights with you.</p>
+                    <input type="email" name="email" value="" placeholder="Your e-mail address">
+                    <input type="submit" name="submit" value="Submit"> 
+                </div>          
+            </div>
+        </div>
+        
+        <div class="footer">
+			
+        </div>
+            
+     </div><!------div.container--------->
+     
+     
+     
+     
+     <!-----------------------------------------Javascript---------------------------------------------------->
+     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+     <script type="text/javascript" src="vertical smooth mousewheel scroll/jQuery-Plugin-For-Smooth-Mouse-Scrolling-scrollSpeed/jQuery.scrollSpeed.js"></script>
+     <script src="smoothScroll/smoothscroll.js"></script>
+     
+     <script>
+	 	jQuery.scrollSpeed(100, 1500);
+	 </script>
+     
+     <script>
+	 	var menuBar = document.getElementById("menuBarId");
+		
+		menuBar.addEventListener("animationend", function() {
+			this.style.opacity = '1';
+		});
+	 </script>
+     
+     <script>
+	 	var headerImgContent = document.getElementsByClassName("header-img-content")[0];
+		
+		headerImgContent.addEventListener("animationend", function() {
+			this.style.transform = 'translateY(0)';
+		});
+	 </script>
+     
+     <script>
+	 	//----scroll 660px to see the scaling effect-------
+		
+		window.onscroll = function() {
+			
+			if(document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+				var aboutUsImg = document.getElementsByClassName("about-us-img");
+				aboutUsImg[0].style.animation = 'imgAnimation 1s';	
+				aboutUsImg[1].style.animation = 'imgAnimation 1s';
+				aboutUsImg[2].style.animation = 'imgAnimation 1s';
+				
+				for(var i = 0; i < aboutUsImg.length; i++) {
+					aboutUsImg[i].addEventListener("animationend", function() {
+						this.style.transform = 'scale(1,1)';
+					});
+				}
+			}//------------if(document.body.scrollTop > 160 || document.documentElement.scrollTop > 160)--------
+			
+		
+			var headerImage = document.getElementById("headerImageId");			
+			
+			headerImage.style.top = 90 + -(window.pageYOffset)/2	+ 'px';
+			
+			var learnAboutThemeBehindBcgId = document.getElementById("learnAboutThemeBehindBcgId");
+			learnAboutThemeBehindBcgId.style.top = 440 + -(window.pageYOffset)/5 + 'px';
+			
+			//---------div.points animation after scrolling----------
+			
+			var pointsDiv = document.getElementsByClassName("points");
+			
+			if(window.pageYOffset > 950) {
+				pointsDiv[0].style.animation = 'pointsDivAnimation 1s linear';
+				pointsDiv[0].style.WebkitAnimation = 'pointsDivAnimation 1s linear';
+				pointsDiv[1].style.animation = 'pointsDivAnimation 1s linear 1s';
+				pointsDiv[1].style.WebkitAnimation = 'pointsDivAnimation 1s linear 1s';
+				pointsDiv[2].style.animation = 'pointsDivAnimation 1s linear 2s';
+				pointsDiv[2].style.WebkitAnimation = 'pointsDivAnimation 1s linear 2s';
+				
+				for(var i = 0; i < pointsDiv.length; i++) {
+					pointsDiv[i].addEventListener("animationend", function() {
+						this.style.opacity = '1';	
+					});	
+				}
+			}
+			
+			//---------learn-theme-image flash------------------
+			
+			var learnThemeImage = document.getElementsByClassName("learn-theme-image")[0];
+			
+			if(window.pageYOffset > 950) {
+				learnThemeImage.style.WebkitAnimation = 'learnThemeImageAnimation 3s';
+				learnThemeImage.style.animation = 'learnThemeImageAnimation 3s';
+				
+				learnThemeImage.addEventListener("animationend", learnThemeFlash);
+				learnThemeImage.addEventListener("webkitAnimationEnd", learnThemeFlash);
+				
+				function learnThemeFlash() {
+					learnThemeImage.style.top = '0px';
+					learnThemeImage.style.opacity = '1';
+				}
+			}
+			
+			//-----------div.contact-us animation-----------
+			
+			var somePage = document.getElementsByClassName("some-page")[0];
+			var recentPost = document.getElementsByClassName("recent-post")[0];
+			var email = document.getElementsByClassName("email")[0];
+			var logo = document.getElementsByClassName("logo")[0];
+			
+			if(window.pageYOffset > 2850) {
+				
+				//------------div.some-page Animation---------------
+				somePage.style.webkitAnimation = 'somePageAnimation 1.5s';
+				somePage.style.animation = 'somePageAnimation 1.5s';
+				
+				somePage.addEventListener("webkitAnimationEnd", function(){
+					this.style.left = '0%';
+					this.style.opacity = '1';
+				});
+				
+				somePage.addEventListener("animationend", function(){
+					this.style.left = '0%';
+					this.style.opacity = '1';
+				});
+				
+				//------------div.recent-post Animation----------------
+				recentPost.style.webkitAnimation = 'recentPostAnimation 1.5s';
+				recentPost.style.animation = 'recentPostAnimation 1.5s';
+				
+				recentPost.addEventListener("webkitAnimationEnd", function(){
+					this.style.left = '0%';
+					this.style.opacity = '1';
+				});
+				
+				recentPost.addEventListener("animationend", function(){
+					this.style.left = '0%';
+					this.style.opacity = '1';
+				});
+				
+				//------------div.email Animation----------------
+				
+				email.style.webkitAnimation = 'emailAnimation 1s ease-out 1.5s';
+				email.style.animation = 'emailAnimation 1s ease-out 1.5s';
+				
+				email.addEventListener("webkitAnimationEnd", function(){
+					this.style.left = '0%';
+					this.style.opacity = '1';
+				});
+				
+				email.addEventListener("animationend", function(){
+					this.style.left = '0%';
+					this.style.opacity = '1';
+				});		
+				
+				//------------div.logo Animation----------------
+						
+				logo.style.webkitAnimation = 'logoAnimation 1s ease-out 1.5s';
+				logo.style.animation = 'logoAnimation 1s ease-out 1.5s';
+				
+				logo.addEventListener("webkitAnimationEnd", function(){
+					this.style.left = '0%';
+					this.style.opacity = '1';
+				});
+				
+				logo.addEventListener("animationend", function(){
+					this.style.left = '0%';
+					this.style.opacity = '1';
+				});					
+				
+			} //-------------if(window.pageYOffset > 2850)-----------
+			
+			if(window.pageYOffset > 1600) {
+				var headerOverBcg = document.getElementsByClassName("header-over-bcg")[0];	
+				
+				headerOverBcg.style.webkitAnimation = 'wobble 2.5s';
+				headerOverBcg.style.animation = 'wobble 2.5s';
+			}//----------if(window.pageYOffset > 1600)----------
+			
+			
+		}; //--------window.onscroll----------------
+	 </script>
+     
+     <!----------menu-items-with-transition---------->
+     
+     <script>
+	 	var menuIcon = document.getElementById("menuIconId");
+		var menuItems = document.getElementsByClassName("menu-icon-items");
+		var menuText = document.getElementsByClassName("menuText");
+		
+		menuIcon.addEventListener("click", function() {
+			for(var i = 0; i < menuText.length; i++) {
+				menuText[i].classList.toggle("menu-items-toggle");	
+			}
+			
+			menuItems[0].classList.toggle("menu-toogle");
+		});
+		
+		window.onclick = function(event) {
+			if(event.target.className != "menu-icon") {
+				for(var i = 0; i < menuText.length; i++) {
+					menuText[i].classList.remove("menu-items-toggle");	
+				}
+				
+				menuItems[0].classList.remove("menu-toogle");			
+			}
+		}
+	 </script>
+
+</body>
+</html>
